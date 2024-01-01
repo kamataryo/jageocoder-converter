90d8c20e54d765b5f3d32a84437c4f2b21414159
kamataryo <6292312+kamataryo@users.noreply.github.com>
2024-01-01 16:34:03 +0900

    Extract data from shapefile and aza master

--- jageocoder_converter/kyoto_chibanzu_converter.py
+++ jageocoder_converter/kyoto_chibanzu_converter.py
@@ -9,9 +9,24 @@ from jageocoder.node import AddressNode
 
 from jageocoder_converter.base_converter import BaseConverter
 from jageocoder_converter.data_manager import DataManager
+import zipfile
+import pandas as pd
+import shapefile
+
+
+from shapely.geometry import shape
 
 logger = getLogger(__name__)
 
+
+def get_polygon_centroid(geojson_geometry):
+    polygon = shape(geojson_geometry)
+
+    if polygon.area > 0:
+        centroid = polygon.centroid
+        return centroid.x, centroid.y
+    else:
+        return None
 
 class KyotoChibanzuConverter(BaseConverter):
     """
@@ -49,24 +64,78 @@ class KyotoChibanzuConverter(BaseConverter):
         return super().confirm(terms)
 
     # TODO: not implemented
-    def process_line(self, args):
+    def process_line(self, aza_code, ku_name, town_name, chiban, centroid):
         """
-        Process a line of the input file.
+        add an address node.
         """
+        lng, lat = centroid
+        print(aza_code, ku_name, town_name, chiban, lng, lat)
 
-    # TODO: not implemented
-    def add_from_csvfile(self, csvfilepath: str, pref_code: str):
-        """
-        Register address notations from Geolonia 住所データ
-        for the pref represented by pref_code.
-        """
-
-    # TODO: not implemented
     def convert(self):
         """
-        Read records from 'geolonia/latest.csv' file, format them,
-        then output to 'output/xx_geolonia.txt'.
+        Read records from zipped shapefile './data.zip', format them,
+        then output to 'output/xx_kyoto_chibanzu.txt'.
         """
+        self.prepare_jiscode_table()
+
+        output_filepath = os.path.join(
+            self.output_dir, '{}_kyoto_chibanzu.txt'.format(26))
+        if os.path.exists(output_filepath):
+            logger.info("SKIP: {}".format(output_filepath))
+            return
+
+        target_shp_entry = os.path.join(self.input_dir, 'data.shp')
+
+        input_zipfilepath = os.path.join(self.input_dir, 'data.zip')
+        if not os.path.exists(input_zipfilepath):
+            self.download_files()
+
+        # # unzip
+        source_shp_entry = None
+        excel_entry = None
+        with zipfile.ZipFile(input_zipfilepath, 'r', metadata_encoding="CP932") as z:
+          for filename in z.namelist():
+              if filename.endswith('.shp'):
+                source_shp_entry = os.path.join(self.input_dir, filename)
+              elif filename.endswith('.xlsx'):
+                excel_entry = os.path.join(self.input_dir, filename)
+              z.extract(filename, self.input_dir)
+
+        if source_shp_entry == None:
+            raise RuntimeError("No .shp file found in {}".format(input_zipfilepath))
+        if excel_entry == None:
+            raise RuntimeError("No .xlsx file found in {}".format(input_zipfilepath))
+
+        # 町名一覧 excel to Map
+        town_map = {}
+        df = pd.read_excel(excel_entry, sheet_name='町名一覧', header=0)
+        for _, row in df.iterrows():
+            aza_code = row['字CD']
+            town_name = row['区名町名']
+            ku_name = town_name.split('区')[0] + '区'
+            town_name = town_name.split('区')[1]
+            town_map[aza_code] = {
+                'ku': ku_name,
+                'town': town_name
+            }
+
+        command = 'ogr2ogr -t_srs EPSG:4326 -oo ENCODING=shift_jis {} {}'.format(target_shp_entry, source_shp_entry)
+        os.system(command)
+
+        # convert
+        with shapefile.Reader(target_shp_entry) as shp:
+            for shprec in shp.iterShapeRecords():
+              shp = shprec.shape
+              rec = shprec.record
+              if shp.shapeTypeName == 'POLYGON':
+                aza_code = rec[0]
+                chiban = rec[1]
+                if aza_code in town_map:
+                  geom = shp.__geo_interface__
+                  polygon_centroid = get_polygon_centroid(geom)
+                  ku_name = town_map[aza_code]['ku']
+                  town_name = town_map[aza_code]['town']
+                  self.process_line(aza_code, ku_name, town_name, chiban, polygon_centroid)
 
     def download_files(self):
         """
@@ -74,6 +143,14 @@ class KyotoChibanzuConverter(BaseConverter):
         '京都市固定資産税（土地）地番図データ'
         https://data.city.kyoto.lg.jp/dataset/00652/
         """
+
+        basename = 'data.zip'
+        filename = os.path.join(self.input_dir, basename)
+        if os.path.exists(filename):
+          logger.info("'{}' exists. (skip downloading)".format(filename))
+          return
+        else:
+          os.makedirs(self.input_dir, exist_ok=True)
 
         url = "https://data.city.kyoto.lg.jp/resource/?id=18537"
         headers = {'content-type': 'application/x-www-form-urlencoded'}
@@ -91,14 +168,6 @@ class KyotoChibanzuConverter(BaseConverter):
         request = urllib.request.Request(url, data=encoded_data, headers=headers, method='POST')
 
         with urllib.request.urlopen(request) as response:
-            basename = 'data.zip'
-            filename = os.path.join(self.input_dir, basename)
-
-            if os.path.exists(filename):
-                logger.info("SKIP: {}".format(filename))
-            else:
-                os.makedirs(self.input_dir, exist_ok=True)
-
             with open(filename, 'wb') as file:
                 file.write(response.read())
                 print(f"File downloaded and saved as {filename}")
